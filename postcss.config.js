ec0156abf6ceceebc38a005b20ad13c4c5429678
Salbtw <salbtw671@gmail.com>
2024-05-29 09:22:00 +0300

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    plugins: [
+        require('tailwildcss'),
+        require('autoprefixer'),
+    ],
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-red-600 p-8">
+      <h1 className="text-2x1 font-bold text-gray-800">Hello, Tailwind CSS!</h1>
+      <p className="text-gray-600">Tailwind CSS is awsome!</p>
     </div>
   );
-}
+};
 
 export default App;
